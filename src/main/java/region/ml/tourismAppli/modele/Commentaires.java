4fa4e309f1209b529652ee3e396191cad481d1d5
kdiawara96 <karimdiawara96@gmail.com>
2022-12-19 18:07:17 +0000

    integrer spring security

--- src/main/java/region/ml/tourismAppli/modele/Commentaires.java
+++ src/main/java/region/ml/tourismAppli/modele/Commentaires.java
@@ -1,41 +1,33 @@
 package region.ml.tourismAppli.modele;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
 
 
+
 @Entity
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class Commentaires {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "description")
     private String description;
 
-    private int aime = 0;
+    private int aimer = 0;
 
 
    //Un commentaire est fait par un et un seul utilisateur
-
     @ManyToOne
     private Utilisateurs utilisateur;
 
     //un commentaire concerne une et une seule région
-
     @ManyToOne
     private Region region;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
